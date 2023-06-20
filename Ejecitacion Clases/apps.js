1367ce5b9b128a2ebd7771141c2f47b5709c06db
TIC97 <thomysurb1997@gmail.com>
2023-06-20 19:48:28 -0300

    Tarea clase 3

--- Ejecitacion Clases/apps.js
+++ Ejecitacion Clases/apps.js
@@ -14,37 +14,3 @@ const randomEmail = chance.email();
 console.log('Nombre Aleatorio ', randomName);
 console.log('Edad Aleatorio ', randomAge);
 console.log('Email Aleatorio ', randomEmail);
-
-
-
-// * FUNCION REGISTRO DE ESTUDIANTES
-
-const readLineSync = require('readline-sync');
-
-const {mostrarListaEstudiantes} = require('../Modulos/modulos');
-
-
-
-const registrarEstudiantes = () => {
-
-    const cantidadEstudiantes = readLineSync.question(` Ingrese la cantidad de estudiantes a registrar:`);
-
-    const estudiantes = [];
-
-    for (let i = 0; i < cantidadEstudiantes; i++) {
-        const nombre = readLineSync.question(`Ingrese el  nombre del estudiante ${i}:`);
-        const edad = readLineSync.question(`Ingrese la edad de ${nombre}:`);
-
-        const estudiante = {
-            nombre,
-            edad
-        };;
-
-        estudiantes.push(estudiante);
-    }
-    mostrarListaEstudiantes(estudiantes);
-}
-
-// * INICIAR REGISTRO DE ESTUDIANTES
-
-registrarEstudiantes();
